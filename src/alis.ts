a0c1ec7b2f6935fa1bcac284eac29cc26b03d881
Md Mehedi Hasan Utsobe <mmhutsobe01@gmail.com>
2023-07-26 01:42:25 +0800

    adding alis and optional types

---
+++ src/alis.ts
@@ -0,0 +1,38 @@
+type CrushType = {
+    name: string;
+    age?: number;
+    profession: string;
+    address: string;
+};
+
+const crush1: CrushType = {
+    name: 'Moyna Pakhi',
+    age: 22,
+    profession: 'Web Developer',
+    address: 'Uganda',
+}
+
+const crush2: CrushType = {
+    name: 'Tia Pakshi',
+    profession: 'Next level web developer',
+    address: 'mexico',
+}
+
+
+type CrushMarriedType = boolean;
+
+const isCrushMarried: CrushMarriedType = false;
+
+type CourseNameType = string;
+
+const courseName: CourseNameType = 'Next Level Web Development';
+
+type OperationType = (x: number, y: number) => number;
+
+const calculate = (number1: number, number2: number, operation: OperationType) => {
+    return operation(number1, number2);
+}
+
+calculate(10, 20, (x, y) => x + y);
+calculate(10, 20, (x, y) => x - y);
+calculate(10, 20, (x, y) => x * y);
