ca9b73f2a145474d638afed731d52f86d647e7f7
Md Mehedi Hasan Utsobe <mmhutsobe01@gmail.com>
2023-07-26 01:04:54 +0800

    funtion module completed

--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,34 @@
 // normal function
 
-function add(num1: number, num2: number): number {
+function add(num1: number, num2: number = 10): number {
     return num1 + num2;
 }
 
-add(2, 2);
+add(30, 50);
+
+const myFriends = ['chandler', 'joey', 'ross'];
+const newFriends = ['monica', 'rachel', 'phobe'];
+const myBestFriend = {
+    fullName: 'Abdul Bashar',
+    age: 24,
+}
+
+const [bestfriend] = myFriends;
+const { fullName: myFullName } = myBestFriend;
+
+myFriends.push(...newFriends);
+console.log(myFriends);
+
+// rest parameter
+
+const greetFriends = (...friends: string[]): void => friends.forEach((friend) => console.log(`Hi ${friend}`)
+);
+
+greetFriends('kashem', 'hashem', 'gashem', 'lahsem', 'bangla bhai', 'English bhai',);
+
+// array and object destructuring
+
+
 
 const addArrow = (num1: number, num2: number): number => num1 + num2;
 
