b952883bb0e8e6cde59997d7fbe59297d2043905
Md Mehedi Hasan Utsobe <mmhutsobe01@gmail.com>
2023-07-29 22:58:37 +0800

    practise code uploaded

---
+++ src/module3/generic-constrains.ts
@@ -0,0 +1,29 @@
+// const newData = { ...myInfo, crush };
+
+interface MandetoryInterface { name: string, age: number, salary: number };
+
+const addMeInMyCrushMind = <T extends MandetoryInterface>(myInfo: T) => {
+    const crush = 'Kate Winslet';
+    const newData = { ...myInfo, crush };
+    return newData;
+}
+
+type MyInfoType = {
+    name: string;
+    age: number;
+    salary: number;
+    other1: boolean;
+    other2: null;
+}
+
+const myInfo = {
+    name: 'Persian',
+    age: 100,
+    salary: 1000000,
+    other1: false,
+    other2: null
+}
+
+const result5 = addMeInMyCrushMind(myInfo);
+
+result5.
--- src/module3/generic-type.ts
+++ src/module3/generic-type.ts
@@ -1,28 +1,75 @@
-type GenericTuple<X, Y> = [X, Y];
+// // Generic Interface
 
-const relation: GenericTuple<string, string> = ['Persian', 'Kate Winslet'];
+// interface CrushInterface<T, U = null> {
+//     name: string;
+//     husband: T;
+//     wife?: U;
+// }
 
-type RelationWithSalaryType = { name: string, salary: number, };
+// interface PersonInterface { name: string, age: number, };
 
-const relationWithSalary: GenericTuple<RelationWithSalaryType, string> = [{
-    name: 'Persian',
-    salary: 1000000,
-},
-    'Kate Winslet'];
+// const crush4: CrushInterface<PersonInterface, PersonInterface> = {
+//     name: 'Kate',
+//     husband: {
+//         name: 'Persian',
+//         age: 30,
+//     },
+//     wife: {
+//         name: 'Winslet',
+//         age: 40,
+//     }
+// }
 
-const relationWithSalary2: GenericTuple<RelationWithSalaryType, string> = [{
-    name: 'Persian',
-    salary: 1000000,
-},
-    'Kate Winslet'];
+// const crush1: CrushInterface<boolean, string> = {
+//     name: 'Kate Winslet', husband: true, wife: 'Sokina',
+// };
 
-type GenericArray<T> = Array<T>;
+// const crush2: CrushInterface<string> = {
+//     name: "kate Winslet", husband: 'Persian',
+// };
+
+// interface HusbandInterface {
+//     name: string,
+//     salary: number,
+// }
+
+// const crush3: CrushInterface<HusbandInterface> = {
+//     name: 'Kate Winslet',
+//     husband: {
+//         name: 'Persian',
+//         salary: 0.01,
+//     }
+// }
+
+// type GenericTuple<X, Y> = [X, Y];
+
+// const relation: GenericTuple<string, string> = ['Persian', 'Kate Winslet'];
+
+// // type RelationWithSalaryType = { name: string, salary: number, };
+// interface RelationWithSalaryInterface {
+//     name: string;
+//     salary: number;
+// }
+
+// const relationWithSalary: GenericTuple<RelationWithSalaryInterface, string> = [{
+//     name: 'Persian',
+//     salary: 1000000,
+// },
+//     'Kate Winslet'];
+
+// const relationWithSalary2: GenericTuple<RelationWithSalaryType, string> = [{
+//     name: 'Persian',
+//     salary: 1000000,
+// },
+//     'Kate Winslet'];
+
+// type GenericArray<T> = Array<T>;
 
 
-const rollNumbers: GenericArray<number> = [44, 12, 4];
-const rollNumbers2: GenericArray<string> = ['44', '12', '4'];
-const rollNumbers3: GenericArray<boolean> = [true, false];
+// const rollNumbers: GenericArray<number> = [44, 12, 4];
+// const rollNumbers2: GenericArray<string> = ['44', '12', '4'];
+// const rollNumbers3: GenericArray<boolean> = [true, false];
 
-type NameRollType = { name: string; roll: number }
+// type NameRollType = { name: string; roll: number }
 
-const userNameAndRollNumbers: GenericArray<NameRollType> = [{ name: 'Mr. X', roll: 1, }, { name: 'Mr. Y', roll: 2, }];
+// const userNameAndRollNumbers: GenericArray<NameRollType> = [{ name: 'Mr. X', roll: 1, }, { name: 'Mr. Y', roll: 2, }];
