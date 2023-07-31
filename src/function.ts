065582ed2ba7ed41eb159c7728bae2d6b2512abe
Md Mehedi Hasan Utsobe <mmhutsobe01@gmail.com>
2023-07-31 11:17:09 +0800

    latest code update

--- src/function.ts
+++ src/function.ts
@@ -1,37 +1,37 @@
-// normal function
+// // normal function
 
-function add(num1: number, num2: number = 10): number {
-    return num1 + num2;
-}
+// function add(num1: number, num2: number = 10): number {
+//     return num1 + num2;
+// }
 
-add(30, 50);
+// add(30, 50);
 
-const myFriends = ['chandler', 'joey', 'ross'];
-const newFriends = ['monica', 'rachel', 'phobe'];
-const myBestFriend = {
-    fullName: 'Abdul Bashar',
-    age: 24,
-}
+// const myFriends = ['chandler', 'joey', 'ross'];
+// const newFriends = ['monica', 'rachel', 'phobe'];
+// const myBestFriend = {
+//     fullName: 'Abdul Bashar',
+//     age: 24,
+// }
 
-const [bestfriend] = myFriends;
-const { fullName: myFullName } = myBestFriend;
+// const [bestfriend] = myFriends;
+// const { fullName: myFullName } = myBestFriend;
 
-myFriends.push(...newFriends);
-console.log(myFriends);
+// myFriends.push(...newFriends);
+// console.log(myFriends);
 
-// rest parameter
+// // rest parameter
 
-const greetFriends = (...friends: string[]): void => friends.forEach((friend) => console.log(`Hi ${friend}`)
-);
+// const greetFriends = (...friends: string[]): void => friends.forEach((friend) => console.log(`Hi ${friend}`)
+// );
 
-greetFriends('kashem', 'hashem', 'gashem', 'lahsem', 'bangla bhai', 'English bhai',);
+// greetFriends('kashem', 'hashem', 'gashem', 'lahsem', 'bangla bhai', 'English bhai',);
 
-// array and object destructuring
+// // array and object destructuring
 
 
 
-const addArrow = (num1: number, num2: number): number => num1 + num2;
+// const addArrow = (num1: number, num2: number): number => num1 + num2;
 
-const arr = [1, 4, 5, 7]
+// const arr = [1, 4, 5, 7]
 
-const newArray = arr.map((elem: number) => elem * elem);
+// const newArray = arr.map((elem: number) => elem * elem);
---
+++ src/module3/conditional-type.ts
@@ -0,0 +1,31 @@
+// // a type is dependent on another type
+
+// type a1 = number;
+// type a3 = undefined;
+// type a4 = number;
+
+
+// type a2 = a1 extends string ? string : null;
+
+// // nested conditional type
+// type d = a1 extends null ? null : a3 extends number ? number : a4 extends null ? null : never;
+
+// type Sheikh = {
+//     wife1: string;
+//     wife2: string;
+// }
+
+// type A = keyof Sheikh;
+
+// type CheckProperty<T, K> = K extends keyof T ? true : false;
+
+// type CheckWife2 = CheckProperty<Sheikh, 'wife1'>;
+
+
+// // Matha Kharap Example
+
+// type Bandhubi = 'Monika' | 'Rachel' | 'Pheobe';
+
+// type RemoveBandhubi<T, K> = T extends K ? never : T;
+
+// type CurrentBandhubi = RemoveBandhubi<Bandhubi, 'Rachel'>;
